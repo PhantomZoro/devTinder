01c96c3b2eb8418ec60dbbb50b487905a2d1fa96
PhantomZoro <phaneendrab.iitm@outlook.com>
2025-05-17 13:41:58 +0530

    Route handlers

--- src/app.js
+++ src/app.js
@@ -2,29 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) =>{
-    res.send("Hello from test route server")
-});
-
-//Handle only get calls
-app.get("/user", (req, res) =>{
-    res.send({
-        firstName: "Phaneendra", 
-        secondName:"Bheesetti"
-    })
-})
-
-//Handle only POST calls 
-app.post("/user", (req, res) =>{
-    //Saving data to db 
-    res.send("Data successfully saved to database")
-})
-
-//Handle only delete calls
-app.delete("/user", (req, res) =>{
-    //Deleting a user 
-    res.send("Deleted a user from database")
-} )
+app.use(
+    "/user", 
+    (req, res, next)=>{
+        //Route Handler 1
+        next();
+        //res.send("Response from Handler 1");
+    },
+    (req, res) =>{
+        //Route Handler 2
+        res.send("Response from handler 2")
+    }        
+)
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000")
